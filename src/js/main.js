bbd578a7475996fdeaaf757ac7a1e7745cb04b4b
Jonas Mohr <jonas.mohr@dfki.de>
2018-04-30 12:01:46 +0200

    integrated MQTT support

--- src/js/main.js
+++ src/js/main.js
@@ -70,3 +70,45 @@ function AppViewModel() {
 }
 
 ko.applyBindings(new AppViewModel());
+
+
+//MQTT
+
+var mqttConfig = {
+    hostname: "broker.mqttdashboard.com",
+    port: 8000,
+    clientID: "client-Z3M4"
+};
+
+// Create a client instance
+client = new Paho.MQTT.Client(mqttConfig.hostname, Number(mqttConfig.port), mqttConfig.clientID);
+
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+// connect the client
+client.connect({onSuccess:onConnect});
+
+
+// called when the client connects
+function onConnect() {
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect");
+    client.subscribe("World");
+    message = new Paho.MQTT.Message("Hello");
+    message.destinationName = "World";
+    client.send(message);
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:"+responseObject.errorMessage);
+    }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+    console.log("onMessageArrived:"+message.payloadString);
+}
