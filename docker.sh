329741083421789ea54a026c1fd0d966eb576abc
jomo02 <jonas.mohr@dfki.de>
2019-05-13 12:06:51 +0200

    changed app name

--- docker.sh
+++ docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker stop rs_webapp
-docker rm rs_webapp
-docker build -t jomo02/robot-status-webapp .
+docker stop component_dashboard
+docker rm component_dashboard
+docker build -t jomo02/component-dashboard .
 docker run \
 	-p 80:8080 \
 	-e BASYS_REST_URL='http://10.2.10.3:8080' \
@@ -9,7 +9,7 @@ docker run \
 	-e MQTT_BROKER_PORT='9001' \
 	-e CAMUNDA_REST_URL='http://10.2.10.4:8080' \
 	--restart unless-stopped \
-	--name rs_webapp \
+	--name component_dashboard \
 	-d \
-	jomo02/robot-status-webapp
+	jomo02/component-dashboard
 
